bd948f588cbaab80fcb5f53f4d1982f6e5fcf4e8
Kazuki Hashimoto <eru.tndl@gmail.com>
2022-07-23 15:42:44 +0900

    :tada: 測定スクリプトを追加

---
+++ co2_mini/sensor.py
@@ -0,0 +1,12 @@
+import co2meter as co2
+
+
+def main() -> None:
+    mon = co2.CO2monitor(bypass_decrypt=True)
+    data = mon.read_data()
+    # 1: co2, 2: temp
+    print(data[1], data[2])
+
+
+if __name__ == "__main__":
+    main()
